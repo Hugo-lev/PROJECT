5e903d14ef4f31e01a2b1df571ec2141f37e2a18
Hugo <levssshe>
2026-08-27 18:52:41 +0900

    feat: develop author router

--- server.js
+++ server.js
@@ -1,35 +1,45 @@
-console.log ("web server start");
+console.log("Web Serverni boshlash");
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const http = require("http");
+const fs = require("fs");
 
-//1 krish
-// 1 shart expressga krib kelayotgan malumotlarga oid bolgan kodlar yoziladi.
-app.use(express.static("public")); //public = appga krib kelayotgan zaproslar public folderni kora oladi degan manoni anglatadi
-app.use(express.json()); //json bu yerda krib kelayotgan datalarni json formatga ogrib beradi.
-app.use(express.urlencoded({extended: true})); 
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if (err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data);
+    }
+});
 
-// 2 session code
+// 1: Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-//3 backanda view yasaymiz viewsga oid kodlar
+// 2: Session code
+// 3: Views code
 app.set("views", "views");
 app.set("view engine", "ejs");
 
-//4 rooterlaarga mojalangan codelar
-app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    res.json({test:"succsses"})
+//4 routing code
+app.post("/create-item",(req,res) => {
+
 });
-app.get("/", function (req, res){
-    res.render("harid");
-})
+
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
+});
+
+app.get("/", function (req, res) {
+  res.render("harid");
+});
+
 
 const server = http.createServer(app);
 let PORT = 3000;
-server.listen(PORT, function(){
-    console.log(`the server is running successfully on port: ${PORT}`);
+server.listen(PORT, function () {
+    console.log(`Server is running: ${PORT}`);
 });
-
-//biz bu yerda backendni ichida ejs framework yordamida frontet qurish va traditional usuli bor
-
-
